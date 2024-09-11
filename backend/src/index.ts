76d574ebae0b17ac713e8c86db0119ff7b890e27
Anish <work.anish.004@gmail.com>
2024-09-11 19:05:00 +0530

    created signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,43 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import bcrypt from 'bcryptjs'
+import { decode, sign, verify } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+    JWT_SECRET: string
+	}
+}>()
 
-app.post('/api/v1/signup', (c) => {
-	return c.text('signup route')
-})
+
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json()
+  const {email,password} = body
+
+  try{
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password,salt)
+
+    const user = await prisma.user.create({
+      data:{
+        email,
+        password: hashedPassword
+      }
+    })
+    const userId = user.id
+    const jwt_secret = c.env.JWT_SECRET
+    const token = await sign({id:userId},jwt_secret)
+
+    return c.json({ message: 'User created successfully', token }, 201)
+  }catch(error){
+   return c.json({ error: 'Error creating user' }, 403)
+  
+}})
 
 app.post('/api/v1/signin', (c) => {
 	return c.text('signin route')
